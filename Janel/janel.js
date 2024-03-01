4c6d950463c4bcfed81c812af5eeda9a6b95eac1
janel.hocde <janel.hocde@orange.fr>
2024-03-01 13:57:24 +0000

    suite de la suite de la suite

--- Janel/janel.js
+++ Janel/janel.js
@@ -1,2 +1,11 @@
-document.querySelector('.photo').style.opacity = "1"
-document.querySelector('.photo').style.transform = "translate(0)"
+document.addEventListener("DOMContentLoaded", function() {
+    let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+
+
+    scrollToTopBtn.addEventListener("click", function() {
+        document.documentElement.scrollTop = 0;
+    });
+    document.querySelector('.photo').style.opacity = "1"
+    document.querySelector('.photo').style.transform = "translate(0)"
+})
